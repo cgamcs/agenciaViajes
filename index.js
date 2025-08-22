51545b5ec7b6b71599654fd7ce37c90c4b3d8602
César García <cesar.garciamcs@outlook.com>
2025-08-22 12:55:26 -0600

    agregando middleware

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ const port = process.env.PORT || 4000
 // Habilitar pug
 app.set('view engine', 'pug')
 
+app.use((req, res, next) => {
+    const year = new Date()
+    res.locals.fecha = year.getFullYear()
+
+    next()
+})
+
 // Definir la carpeta publica
 app.use(express.static('public'))
 
